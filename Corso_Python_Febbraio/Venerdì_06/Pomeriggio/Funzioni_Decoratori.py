b1f3804719613615e65fe8371a44b09534d42029
eli-carella <eli.carella@gmail.com>
2026-02-08 17:42:09 +0100

    Add files via upload

---
+++ Corso_Python_Febbraio/Venerdì_06/Pomeriggio/Funzioni_Decoratori.py
@@ -0,0 +1,67 @@
+def fibonacci(n):
+    a, b = 0, 1
+    while a < n:
+        yield a
+        a, b = b, a + b
+
+print(list(fibonacci(10)))
+
+def decoratore(funzione):
+    def wrapper():
+        print("Prima dell'esecuzione della funzione")
+        funzione()
+        print("Dopo l'esecuzione della funzione")
+    return wrapper
+
+@decoratore
+def saluta():
+    print("Ciao!")
+saluta()
+
+def decoratore_con_argomenti(funzione):
+    def wrapper(*args, **kwargs):
+        print("Prima")
+        risultato = funzione(*args, **kwargs)
+        print("Dopo")
+        return risultato
+    return wrapper
+
+@decoratore_con_argomenti
+def somma(a, b):
+    print(a+b)
+    return a + b
+
+print("risultato è ", somma(3, 4))
+
+def logger(funzione):
+    def wrapper(*args, **kwargs):
+        print(f"Chiamata a {funzione.__name__} con argomenti: {args} e {kwargs}")
+        risultato = funzione(*args, **kwargs)
+        print(f"Risultato di {funzione.__name__}: {risultato}")
+        return risultato
+    return wrapper
+
+@logger
+def moltiplica(a, b):
+    return a * b
+
+# Chiamata alla funzione decorata
+print(moltiplica(3, 4))
+
+
+import time
+def calcola_tempo(funzione):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        risultato = funzione(*args, **kwargs)
+        end_time = time.time()
+        print(f"Tempo di esecuzione: {end_time - start_time} secondi")
+        return risultato
+    return wrapper
+
+@calcola_tempo
+def calcolo_lento():
+    time.sleep(2)
+    print("Calcolo completato")
+# Chiamata alla funzione decorata
+calcolo_lento()
